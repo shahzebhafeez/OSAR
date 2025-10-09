78106f5dc197ee2f181565b341e6cb6a20e4e8b6
shahzebhafeez <shahzebhafeez123@gmail.com>
2025-10-09 20:46:48 +0500

    removed code

--- tests/test_integration.py
+++
@@ -1,6 +0,0 @@
-from src.main import simulate
-
-def test_small_simulation_runs():
-    avg_d, pdr, paths = simulate(N=5, num_packets=2, M=2, visualize=False)
-    assert avg_d > 0, "Average delay should be positive"
-    assert 0 <= pdr <= 1, "PDR must be within [0, 1]"
